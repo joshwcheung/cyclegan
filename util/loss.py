cf0b422749600dbc05668901caa23a2d04583aeb
Joshua Cheung <joshua.cheung@usc.edu>
2017-11-13 20:40:51 -0800

    Rename losses

--- util/loss.py
+++ util/loss.py
@@ -3,10 +3,10 @@ import tensorflow as tf
 def cycle_consistency_loss(real, cycle):
     return tf.reduce_mean(tf.abs(real - cycle))
 
-def generator_loss(fake):
+def lsgan_generator_loss(fake):
     return tf.reduce_mean(tf.squared_difference(fake, 1))
 
-def discriminator_loss(real, fake):
+def lsgan_discriminator_loss(real, fake):
     return (tf.reduce_mean(tf.squared_difference(real, 1)) + 
             tf.reduce_mean(tf.squared_difference(fake, 0))) * 0.5
 
