adecbb1a3b20c2d5b2c63fed60e7774b4da60636
Joshua Cheung <joshua.cheung@usc.edu>
2017-11-13 20:58:24 -0800

    Define optimizer step

--- util/cyclegan.py
+++ util/cyclegan.py
@@ -59,3 +59,35 @@ class CycleGan:
             self.p_fake_pool_a = discriminator(self.fake_pool_a, 'd_a')
             self.p_fake_pool_b = discriminator(self.fake_pool_b, 'd_b')
     
+    def opt(self):
+        cyclic_loss_a = self.lambda_a * cyclic_loss(self.real_a, self.cycle_a)
+        cyclic_loss_b = self.lambda_b * cyclic_loss(self.real_b, self.cycle_b)
+        
+        lsgan_loss_a = lsgan_gen_loss(self.p_fake_a)
+        lsgan_loss_b = lsgan_gen_loss(self.p_fake_b)
+        
+        g_a_loss = cyclic_loss_a + cyclic_loss_b + lsgan_loss_b
+        g_b_loss = cyclic_loss_b + cyclic_loss_a + lsgan_loss_a
+        
+        d_a_loss = lsgan_dis_loss(self.p_real_a, self.p_fake_pool_a)
+        d_b_loss = lsgan_dis_loss(self.p_real_b, self.p_fake_pool_b)
+        
+        optimizer = tf.train.AdamOptimizer(self.lr, beta1=0.5)
+        
+        self.vars = tf.trainable_variables()
+        d_a_vars = [var for v in self.vars if 'd_a' in v.name]
+        d_b_vars = [var for v in self.vars if 'd_b' in v.name]
+        g_a_vars = [var for v in self.vars if 'g_a' in v.name]
+        g_b_vars = [var for v in self.vars if 'g_b' in v.name]
+        
+        self.d_a_train = optimizer.minimize(d_a_loss, var_list=d_a_vars)
+        self.d_b_train = optimizer.minimize(d_b_loss, var_list=d_b_vars)
+        self.g_a_train = optimizer.minimize(g_a_loss, var_list=g_a_vars)
+        self.g_b_train = optimizer.minimize(g_b_loss, var_list=g_b_vars)
+        
+        self.d_a_loss_summary = tf.summary.scalar('d_a_loss', d_a_loss)
+        self.d_b_loss_summary = tf.summary.scalar('d_b_loss', d_b_loss)
+        self.g_a_loss_summary = tf.summary.scalar('g_a_loss', g_a_loss)
+        self.g_b_loss_summary = tf.summary.scalar('g_b_loss', g_b_loss)
+    
+    
