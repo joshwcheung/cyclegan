21e5f5a9ad60af4777a9d5c17f78540c3157ad54
Joshua Cheung <joshua.cheung@usc.edu>
2017-11-15 21:16:36 -0800

    Write nifti to npy converter

---
+++ util/nifti_to_npy.py
@@ -0,0 +1,31 @@
+import nibabel as nib
+import numpy as np
+import os
+
+nifti_path = '../datasets/gad/nifti/'
+npy_path = '../datasets/gad/npy/'
+
+for x in ('pre', 'post'):
+    for filename in os.listdir(os.path.join(nifti_path, x)):
+        path = os.path.join(nifti_path, x, filename)
+        subj_name, ext = os.path.splitext(filename)
+        if ext == '.gz':
+            subj_name = os.path.splitext(subj_name)[0]
+        
+        #Load image
+        img = nib.load(path)
+        affine = img.affine
+        data = img.get_data()
+        
+        #Save affine, image as .npy
+        print('Saving {:s}-{:s}...'.format(x, subj_name))
+        np.save(os.path.join(npy_path, 'affine', x, subj_name), affine)
+        np.save(os.path.join(npy_path, 'whole', x, subj_name), data)
+        
+        #Save slices as .npy
+        for i in range(data.shape[-1]):
+            sl_name = '{:s}-{:03d}'.format(subj_name, i)
+            np.save(os.path.join(npy_path, 'slices', x, sl_name), data[:, :, i])
+
+print('Done.')
+
