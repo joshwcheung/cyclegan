b0a7365363c570e8e519f9660318970cf0e0542e
Joshua Cheung <joshua.cheung@usc.edu>
2017-11-17 07:24:44 -0800

    Simplify nifti_to_py

--- util/nifti_to_npy.py
+++ util/nifti_to_npy.py
@@ -12,6 +12,16 @@ for x in ('pre', 'post'):
         if ext == '.gz':
             subj_name = os.path.splitext(subj_name)[0]
         
+        affine_path = os.path.join(npy_path, 'affine', x)
+        whole_path = os.path.join(npy_path, 'whole', x)
+        slices_path = os.path.join(npy_path, 'slices', x)
+        if not os.path.exists(affine_path):
+            os.makedirs(affine_path)
+        if not os.path.exists(whole_path):
+            os.makedirs(whole_path)
+        if not os.path.exists(slices_path):
+            os.makedirs(slices_path)
+        
         #Load image
         img = nib.load(path)
         affine = img.affine
@@ -25,13 +35,13 @@ for x in ('pre', 'post'):
         
         #Save affine, image as .npy
         print('Saving {:s}-{:s}...'.format(x, subj_name))
-        np.save(os.path.join(npy_path, 'affine', x, subj_name), affine)
-        np.save(os.path.join(npy_path, 'whole', x, subj_name), data)
+        np.save(os.path.join(affine_path, subj_name), affine)
+        np.save(os.path.join(whole_path, subj_name), data)
         
         #Save slices as .npy
         for i in range(data.shape[-1]):
-            sl_name = '{:s}-{:03d}'.format(subj_name, i)
-            np.save(os.path.join(npy_path, 'slices', x, sl_name), 
+            slice_name = '{:s}-{:03d}'.format(subj_name, i)
+            np.save(os.path.join(slices_path, slice_name), 
                     data[:, :, i, :])
 
 print('Done.')
