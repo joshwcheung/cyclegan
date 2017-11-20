27737407d6a55006453f4bdb8c55bc36e8c78ea0
Joshua Cheung <joshua.cheung@usc.edu>
2017-11-20 13:33:35 -0800

    Use read function in nifti_to_record

--- util/cyclegan.py
+++ util/cyclegan.py
@@ -70,31 +70,31 @@ class CycleGAN:
         #image_b = tf.reshape(tf.decode_raw(train_b_file, tf.float32), 
         #                     [self.w, self.h, self.c])
         
-        #image_a = read_from_tfrecord(train_a_names)
-        #image_b = read_from_tfrecord(train_b_names)
+        image_a = read_from_tfrecord(train_a_names)
+        image_b = read_from_tfrecord(train_b_names)
         
-        train_a_queue = tf.train.string_input_producer(train_a_names)
-        train_b_queue = tf.train.string_input_producer(train_b_names)
+        #train_a_queue = tf.train.string_input_producer(train_a_names)
+        #train_b_queue = tf.train.string_input_producer(train_b_names)
         
-        reader = tf.TFRecordReader()
-        _, train_a_file = reader.read(train_a_queue)
-        _, train_b_file = reader.read(train_b_queue)
+        #reader = tf.TFRecordReader()
+        #_, train_a_file = reader.read(train_a_queue)
+        #_, train_b_file = reader.read(train_b_queue)
         
-        features = {'shape': tf.FixedLenFeature([], tf.string), 
-                    'array': tf.FixedLenFeature([], tf.string)}
-        features_a = tf.parse_single_example(train_a_file, features=features, 
-                                             name='features_a')
-        features_b = tf.parse_single_example(train_b_file, features=features, 
-                                             name='features_b')
+        #features = {'shape': tf.FixedLenFeature([], tf.string), 
+        #            'array': tf.FixedLenFeature([], tf.string)}
+        #features_a = tf.parse_single_example(train_a_file, features=features, 
+        #                                     name='features_a')
+        #features_b = tf.parse_single_example(train_b_file, features=features, 
+        #                                     name='features_b')
         
-        image_a = tf.decode_raw(features_a['array'], tf.float32)
-        image_b = tf.decode_raw(features_b['array'], tf.float32)
+        #image_a = tf.decode_raw(features_a['array'], tf.float32)
+        #image_b = tf.decode_raw(features_b['array'], tf.float32)
         
-        shape_a = tf.decode_raw(features_a['shape'], tf.int32)
-        shape_b = tf.decode_raw(features_b['shape'], tf.int32)
+        #shape_a = tf.decode_raw(features_a['shape'], tf.int32)
+        #shape_b = tf.decode_raw(features_b['shape'], tf.int32)
         
-        image_a = tf.reshape(image_a, shape_a)
-        image_b = tf.reshape(image_b, shape_b)
+        #image_a = tf.reshape(image_a, shape_a)
+        #image_b = tf.reshape(image_b, shape_b)
         
         #Resize without scaling
         self.input_a = tf.image.resize_image_with_crop_or_pad(image_a, 
