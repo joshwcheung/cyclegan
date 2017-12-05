c24b83c9e2b32349f23cecff5901bd76c6ccb1e0
Joshua Cheung <joshua.cheung@usc.edu>
2017-12-04 20:33:34 -0800

    Update training folder name

--- example.sh
+++ example.sh
@@ -3,6 +3,9 @@
 #Training
 python main.py -t -s 1
 
+#Resuming
+#python main.py -t -r -d 20171202-030822 -s 1
+
 #Testing
-python main.py -d 20171122-042600 -i 06 07 08 -j 06 07 08
+#python main.py -d 20171202-030822 -i 06 07 08 -j 06 07 08
 
