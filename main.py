65f278cd2634bfb927b8474fc2be712b3a0fbb1a
Joshua Cheung <joshua.cheung@usc.edu>
2017-12-07 15:31:14 -0800

    Make input values stricter for argparse

--- main.py
+++ main.py
@@ -3,12 +3,33 @@ import os
 
 from models.cyclegan import CycleGAN
 
-def positive(value):
+def positive_int(value):
     ivalue = int(value)
     if ivalue <= 0:
         error = '{:s} is an invalid positive int value'.format(value)
         raise argparse.ArgumentTypeError(error)
     return ivalue
+
+def positive_or_zero_int(value):
+    ivalue = int(value)
+    if ivalue < 0:
+        error = '{:s} must be an int greater than or equal to 0'.format(value)
+        raise argparse.ArgumentTypeError(error)
+    return ivalue
+
+def restricted_int(value):
+    ivalue = int(value)
+    if ivalue < 256:
+        error = '{:s} must be an int greater than or equal to 256'.format(value)
+        raise argparse.ArgumentTypeError(error)
+    return ivalue
+
+def positive_float(value):
+    fvalue = float(value)
+    if fvalue <= 0:
+        error = '{:s} is an invalid positive float value'.format(value)
+        raise argparse.ArgumentTypeError(error)
+    return fvalue
 
 def main():
     parser = argparse.ArgumentParser(description='CycleGAN arguments.')
@@ -34,38 +55,39 @@ def main():
     tr_args = parser.add_argument_group('train', 'Training arguments')
     
     tr_args.add_argument('-W', '--width', action='store', nargs='?', 
-                         default=286, type=positive, 
+                         default=286, type=restricted_int, 
                          help=('Input width for training. Image will be '
-                               'padded/cropped to input width, then randomly '
+                               'resized to input width, then randomly '
                                'cropped to width 256. Default: 286'))
     tr_args.add_argument('-H', '--height', action='store', nargs='?', 
-                         default=286, type=positive, 
+                         default=286, type=restricted_int, 
                          help=('Input height for training. Image will be '
-                               'padded/cropped to input height, then randomly '
+                               'resized to input height, then randomly '
                                'cropped to height 256. Default: 286'))
     tr_args.add_argument('-x', '--lambda1', action='store', nargs='?', 
-                         default=10.0, type=float, 
+                         default=10.0, type=positive_float, 
                          help=('Weight for forward cyclic loss (A -> B -> A). '
                                'Default: 10.0'))
     tr_args.add_argument('-y', '--lambda2', action='store', nargs='?', 
-                         default=10.0, type=float, 
+                         default=10.0, type=positive_float, 
                          help=('Weight for backward cyclic loss (B -> A -> B). '
                                'Default: 10.0'))
     tr_args.add_argument('-p', '--poolsize', action='store', nargs='?', 
-                         default=50, type=positive, 
+                         default=50, type=positive_int, 
                          help=('Number of fake images to store for calculating '
                                'loss. Deault: 50'))
     tr_args.add_argument('-l', '--lr', action='store', nargs='?', 
-                         default=0.0002, type=float, 
+                         default=0.0002, type=positive_float, 
                          help=('Base learning rate. Default: 0.0002'))
     tr_args.add_argument('-e', '--epochs', action='store', nargs='?', 
-                         default=200, type=positive, 
+                         default=200, type=positive_int, 
                          help='Max epochs. Default: 200')
     tr_args.add_argument('-s', '--save', action='store', nargs='?', default=0, 
-                         type=int, help=('Number of training images to save '
-                                         'for random subjects. Default: 0'))
+                         type=positive_or_zero_int, 
+                         help=('Number of training images to save for random '
+                               'subjects. Default: 0'))
     tr_args.add_argument('-b', '--batch', action='store', nargs='?', default=1, 
-                         type=positive, help='Batch size. Default: 1')
+                         type=positive_int, help='Batch size. Default: 1')
     tr_args.add_argument('-r', '--restore', action='store_true', 
                          help=('Restore previous checkpoint. Only used during '
                                'training. Ignore for new training run.'))
